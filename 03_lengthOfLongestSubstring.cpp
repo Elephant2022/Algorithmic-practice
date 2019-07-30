bd3f297c5ffa0990e42ef28eaf40ba9a8d3b40a2
ThorLsl <1262458173@qq.com>
2019-07-30 15:58:58 +0800

    添加代码

---
+++ 03_lengthOfLongestSubstring.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+/*
+ *给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
+ *示例 1:
+ *
+ *输入: "abcabcbb"
+ *输出: 3
+ *解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ *
+ *示例 2:
+ *
+ *输入: "bbbbb"
+ *输出: 1
+ *解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ *
+ *示例 3:
+ *
+ *输入: "pwwkew"
+ *输出: 3
+ *解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *
+ *来源：力扣（LeetCode）
+ *链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ *著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+public:
+	/*无重复字符的最长子串*/
+	int lengthOfLongestSubstring(string s) {
+		string str;
+		int count = 0;
+		for (auto c : s)
+		{
+			int k = str.find_first_of(c);
+			if (std::string::npos == k)
+			{
+				str.push_back(c);
+			}
+			else
+			{
+				count = count < str.size() ? str.size() : count;
+				str.erase(0, k + 1);
+				str.push_back(c);
+			}
+		}
+
+		count = count < str.size() ? str.size() : count;
+		return count;
+	}
+};
+
+int main()
+{
+	string str = "pwwhew";
+	Solution p;
+	int count = p.lengthOfLongestSubstring(str);
+	system("pause");
+	return 0;
+}
---
+++ 04_findMedianSortedArrays.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+#define max(a,b)(((a)>(b))?(a):(b))
+#define min(a,b)(((a)<(b))?(a):(b))
+
+class Solution {
+/*
+ *给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ *你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ *示例 1:
+ *nums1 = [1, 3]
+ *nums2 = [2]
+ *
+ *则中位数是 2.0
+ *
+ *示例 2:
+ *nums1 = [1, 2]
+ *nums2 = [3, 4]
+ *
+ *则中位数是 (2 + 3)/2 = 2.5
+ *
+ *来源：力扣（LeetCode）
+ *链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ *著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+*/
+public:
+	/*寻找两个有序数组的中位数--解法一*/
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+	{
+		int median = (nums1.size() + nums2.size()) / 2;
+
+		int p = 0, q = 0;
+		vector<int>s;
+		for (int i = 0; i < median + 1; i++)
+		{
+			if (q < nums1.size() && p < nums2.size())
+			{
+				if (nums1.at(q) < nums2.at(p))
+				{
+					s.push_back(nums1.at(q));
+					q++;
+				}
+				else
+				{
+					s.push_back(nums2.at(p));
+					p++;
+				}
+			}
+			else if (q < nums1.size())
+			{
+				s.push_back(nums1.at(q));
+				q++;
+			}
+			else if (p < nums2.size())
+			{
+				s.push_back(nums2.at(p));
+				p++;
+			}
+		}
+
+		if ((nums1.size() + nums2.size()) % 2 == 1)
+		{
+			return s.at(median);
+		}
+		else
+		{
+			return (s.at(median) + s.at(median - 1)) / 2.0f;
+		}
+	}
+
+	/*寻找两个有序数组的中位数--解法二（二分法）*/
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+	{
+		int n = nums1.size();
+		int m = nums2.size();
+
+		if (n > m)
+		{
+			return findMedianSortedArrays(nums2, nums1);
+		}
+
+		int Lmax1, Lmax2, Rmin1, Rmin2, c1, c2, lo = 0, hi = 2 * n;
+		while (lo <= hi)
+		{
+			c1 = (lo + hi) / 2;
+			c2 = m + n - c1;
+
+			Lmax1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
+			Rmin1 = (c1 == 2 * n) ? INT_MAX : nums1[c1 / 2];
+			Lmax2 = (c2 == 0) ? INT_MIN : nums2[(c2 - 1) / 2];
+			Rmin2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
+
+			if (Lmax1 > Rmin2)
+			{
+				hi = c1 - 1;
+			}
+			else if (Lmax2 > Rmin1)
+			{
+				lo = c1 + 1;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		return (max(Lmax1, Lmax2) + min(Rmin1, Rmin2)) / 2.0f;
+	}
+};
+
+int main()
+{
+	vector<int> l1 = { 2,7,11,15 };
+	vector<int> l2 = { 2,3,4 };
+	Solution p;
+	int k = p.findMedianSortedArrays(l1, l2);
+	system("pause");
+	return 0;
+}
